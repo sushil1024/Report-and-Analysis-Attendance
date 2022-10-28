5aa493d0965b8bfa144d8d41c683a2100b5d1520
Sushil <sushilwaghmare2048@gmail.com>
2022-10-28 13:02:37 +0530

    minor changes

--- app.py
+++ app.py
@@ -4,11 +4,13 @@
 # Author: Sushil Waghmare
 
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 import mysql.connector
+from datetime import datetime
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'sush1024'
 
 
 con = mysql.connector.connect(
@@ -28,38 +30,38 @@ def home():
 
 
 # authentication page
-@app.route("/login")
+@app.route("/loginstaff")
 def login():
-    return render_template("login.html")
+    return render_template("loginstaff.html")
 
 
 @app.route("/entry", methods=['GET', 'POST'])
-def entry1():
+def entry():
     if request.method == 'POST':
-        # stuname = request.form['stuname']
-        # gender = request.form['gender']
-        # country = request.form['country']
-        # contactno = request.form['contactno']
-        # city = request.form['city']
-        # age = request.form['age']
-        # dob = request.form['dob']
-        # print(type(dob))
-        # print(dob)
-        # emailid = request.form['emailid']
-        #
-        # cur.execute("INSERT INTO STUDENTS SELECT STUID+1, ROLLNO+1, %s, %s, %s, %s, %s, %s, %s, %s, 30 FROM STUDENTS ORDER BY STUID DESC LIMIT 1;", (stuname, gender, country, city, contactno, age, dob, emailid))
-        # con.commit()
-        stuname = "sunil waghmare"
-        gender = "male"
-        country = "india"
-        contactno = "1234567890"
-        city = "mumbai"
-        age = "53"
-        dob = "1969-10-22"
-        emailid = "sushilwaghmare2048@gmail.com"
+        stuname = request.form['stuname']
+        gender = request.form['gender']
+        country = request.form['country']
+        contactno = request.form['contactno']
+        city = request.form['city']
+        age = request.form['age']
+        dob = datetime.datetime.strptime(request.form['dob'], '%Y-%m-%d')
+        emailid = request.form['emailid']
 
         cur.execute("INSERT INTO STUDENTS SELECT STUID+1, ROLLNO+1, %s, %s, %s, %s, %s, %s, DATE_FORMAT(%s, '%d %M %Y'), %s, 30 FROM STUDENTS ORDER BY STUID DESC LIMIT 1;", (stuname, gender, country, city, contactno, age, dob, emailid))
         con.commit()
+
+
+        # stuname = "sunil waghmare"
+        # gender = "male"
+        # country = "india"
+        # contactno = "1234567890"
+        # city = "mumbai"
+        # age = "53"
+        # dob = "1969-10-22"
+        # emailid = "sushilwaghmare2048@gmail.com"
+        #
+        # cur.execute("INSERT INTO STUDENTS SELECT STUID+1, ROLLNO+1, %s, %s, %s, %s, %s, %s, DATE_FORMAT(%s, '%d %M %Y'), %s, 30 FROM STUDENTS ORDER BY STUID DESC LIMIT 1;", (stuname, gender, country, city, contactno, age, dob, emailid))
+        # con.commit()
 
     return render_template("entry.html")
 
