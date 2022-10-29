eea28ff2d87122f87e5fe17d341eda2370ce0dbd
Sushil <sushilwaghmare2048@gmail.com>
2022-10-30 00:13:55 +0530

    entry changes in app.py

--- app.py
+++ app.py
@@ -38,30 +38,30 @@ def login():
 @app.route("/entry", methods=['GET', 'POST'])
 def entry():
     if request.method == 'POST':
-        # stuname = request.form['stuname']
-        # gender = request.form['gender']
-        # country = request.form['country']
-        # contactno = request.form['contactno']
-        # city = request.form['city']
-        # age = request.form['age']
-        # dob = datetime.datetime.strptime(request.form['dob'], '%Y-%m-%d')
-        # emailid = request.form['emailid']
-        #
-        # cur.execute("INSERT INTO STUDENTS SELECT STUID+1, ROLLNO+1, %s, %s, %s, %s, %s, %s, DATE_FORMAT(%s, '%d %M %Y'), %s, 30 FROM STUDENTS ORDER BY STUID DESC LIMIT 1;", (stuname, gender, country, city, contactno, age, dob, emailid))
-        # con.commit()
-
-
-        stuname = "sunil waghmare"
-        gender = "male"
-        country = "india"
-        contactno = "1234567890"
-        city = "mumbai"
-        age = "53"
-        dob = "1969-10-22"
-        emailid = "sushilwaghmare2048@gmail.com"
+        stuname = request.form['stuname']
+        gender = request.form['gender']
+        country = request.form['country']
+        contactno = request.form['contactno']
+        city = request.form['city']
+        age = request.form['age']
+        dob = datetime.datetime.strftime(request.form['dob'], '%Y-%m-%d')
+        emailid = request.form['emailid']
 
         cur.execute("INSERT INTO STUDENTS SELECT STUID+1, ROLLNO+1, %s, %s, %s, %s, %s, %s, DATE_FORMAT(%s, '%d %M %Y'), %s, 30 FROM STUDENTS ORDER BY STUID DESC LIMIT 1;", (stuname, gender, country, city, contactno, age, dob, emailid))
         con.commit()
+
+
+        # stuname = "sunil waghmare"
+        # gender = "male"
+        # country = "india"
+        # contactno = "1234567890"
+        # city = "mumbai"
+        # age = "53"
+        # dob = "1969-10-22"
+        # emailid = "sushilwaghmare2048@gmail.com"
+        #
+        # cur.execute("INSERT INTO STUDENTS SELECT STUID+1, ROLLNO+1, %s, %s, %s, %s, %s, %s, DATE_FORMAT(%s, '%d %M %Y'), %s, 30 FROM STUDENTS ORDER BY STUID DESC LIMIT 1;", (stuname, gender, country, city, contactno, age, dob, emailid))
+        # con.commit()
 
     return render_template("entry.html")
 
